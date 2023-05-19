9d5e0879a5c787a4207c0414721dbbd3932db718
funkelman <basil.ftkc@gmail.com>
2023-05-19 21:46:03 +0200

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ use image::{ImageBuffer, Rgba};
 use chrono::prelude::*;
 
 //  function to generate the icon image  
-//TODO//    icon at index 10n+1 causes thread panic due to image buffer dimension overflow
-//TODO//    because no implementation of wrapping around to the next row
 fn img_generator(txt2dcrypt: &str, s2p_map: &HashMap<char, Vec<char>>) {
     let width = 2000;
     let height = 3000;
