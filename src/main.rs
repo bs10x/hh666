e813ddb0f256b89bd2f63902a6ccdbee43aa2ce0
Basil Feitknecht <bfeitknecht@student.ethz.ch>
2023-10-02 12:52:19 +0200

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use rand::{seq::SliceRandom, SeedableRng};
-use rand::rngs::StdRng;
-use sha3::{Digest, Sha3_256};
-use std::collections::HashSet;
-use std::io::{self, Write};
-use std::collections::HashMap;
+use chrono::prelude::*;
 use clap::Parser;
 use image::{ImageBuffer, Rgba};
-use chrono::prelude::*;
+use rand::rngs::StdRng;
+use rand::{seq::SliceRandom, SeedableRng};
+use sha3::{Digest, Sha3_256};
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::io::{self, Write};
 
-//  function to generate the icon image  
-fn img_generator(txt2dcrypt: &str, s2p_map: &HashMap<char, Vec<char>>) {
+//  function to generate the icon image
+fn img_generator(txt2dcrypt: &str) {
     let width = 2000;
     let height = 3000;
     let icon_width = 200;
@@ -32,12 +32,12 @@ fn img_generator(txt2dcrypt: &str, s2p_map: &HashMap<char, Vec<char>>) {
 
         for (i, c) in icon.chars().enumerate() {
             let icon_color = match c {
-                'c' => Rgba([0, 255, 255, 255]),    // cyan
-                'y' => Rgba([255, 255, 0, 255]),    // yellow
-                'm' => Rgba([255, 0, 255, 255]),    // magenta
-                'r' => Rgba([255, 0, 0, 255]),      // red
-                'g' => Rgba([0, 255, 0, 255]),      // green
-                'b' => Rgba([0, 0, 255, 255]),      // blue
+                'c' => Rgba([0, 255, 255, 255]), // cyan
+                'y' => Rgba([255, 255, 0, 255]), // yellow
+                'm' => Rgba([255, 0, 255, 255]), // magenta
+                'r' => Rgba([255, 0, 0, 255]),   // red
+                'g' => Rgba([0, 255, 0, 255]),   // green
+                'b' => Rgba([0, 0, 255, 255]),   // blue
                 _ => panic!("Invalid icon character"),
             };
 
@@ -63,10 +63,13 @@ fn img_generator(txt2dcrypt: &str, s2p_map: &HashMap<char, Vec<char>>) {
     image.save(&filename).expect("Failed to save image");
 }
 
-
-
 //  generate all 720 permutations of 'chars'
-fn permutation_generator(chars: &[char], used: &mut HashSet<char>, permutor: &mut Vec<char>, permutations: &mut Vec<Vec<char>>) {
+fn permutation_generator(
+    chars: &[char],
+    used: &mut HashSet<char>,
+    permutor: &mut Vec<char>,
+    permutations: &mut Vec<Vec<char>>,
+) {
     if permutor.len() == chars.len() {
         permutations.push(permutor.clone());
         return;
@@ -82,7 +85,6 @@ fn permutation_generator(chars: &[char], used: &mut HashSet<char>, permutor: &mu
     }
 }
 
-
 /// hh666, dynamic pseudorandom 1:1 substitution cipher
 #[derive(Parser, Debug)]
 #[command(name = "hh666", author = "funkelman", version = "0.0.0", about, long_about = None)]
@@ -92,46 +94,41 @@ struct Args {
     key: Option<String>,
 
     /// UserInputStringTXT2NCRYPT
-    #[arg(short, long,)]
+    #[arg(short, long)]
     input: Option<String>,
 }
 
-
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     //??//  initializing clap_parser or smthng, idfk
     let args = Args::parse();
     println!("parsed_arguments: \n{:?}", args);
 
-
     //  read UserInputString "k"
-    let k = args.key.map(|s| s.trim_end().to_owned())
-    .unwrap_or_else(|| {
-        println!("please define k!");
-        print!("k: ");
-        io::stdout().flush().unwrap();
-        let mut k = String::new();
-        match io::stdin().read_line(&mut k) {
-            Ok(_) => k.trim_end().to_owned(),
-            Err(err) => {
-                eprintln!("Error reading UserInputStringKey: {}", err);
-                std::process::exit(1);
+    let k = args
+        .key
+        .map(|s| s.trim_end().to_owned())
+        .unwrap_or_else(|| {
+            println!("please define k!");
+            print!("k: ");
+            io::stdout().flush().unwrap();
+            let mut k = String::new();
+            match io::stdin().read_line(&mut k) {
+                Ok(_) => k.trim_end().to_owned(),
+                Err(err) => {
+                    eprintln!("Error reading UserInputStringKey: {}", err);
+                    std::process::exit(1);
+                }
             }
-        }
-    });
-
+        });
 
     //  vector of the symbols to map
     let symbols = vec![
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_', '.', ',',
-        ';', ':', '!', '?', '&', '@', '#', '$', '§', '≈', '≠', '=', '<',
-        '>', '~', '–', '-', '±', '+', '’', '”', '*', '°', '^', '/', '%',
-        '(', ')', '[', ']', '{', '}', '|', '\t', ' ', '\n'
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1',
+        '2', '3', '4', '5', '6', '7', '8', '9', '_', '.', ',', ';', ':', '!', '?', '&', '@', '#',
+        '$', '§', '≈', '≠', '=', '<', '>', '~', '–', '-', '±', '+', '’', '”', '*', '°', '^', '/',
+        '%', '(', ')', '[', ']', '{', '}', '|', '\t', ' ', '\n',
     ];
 
     //  hash input string "k" w/ SHA3-256
@@ -158,57 +155,61 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut selection = permutations.into_iter().take(101).collect::<Vec<_>>();
     selection.shuffle(&mut prng);
 
+    /*
     //[DBG]//   print the 101 pseudorandom selected elements "p_pr_101"
-    //println!("p_pr_101: ");
-    //for p in &selection {
-    //    println!("{}", p.iter().collect::<String>());
-    //}
+    println!("p_pr_101: ");
+    for p in &selection {
+       println!("{}", p.iter().collect::<String>());
+    }
+    */
 
     //  create a hashmap to store p_pr_101 as values with s_101 as keys
     let mut s2p_map: HashMap<char, Vec<char>> = HashMap::new();
 
     //  iterate through the symbols and assign a selected permutation to each one
     for (symbol, permutation) in symbols.iter().zip(selection.iter()) {
-    s2p_map.insert(*symbol, permutation.to_owned());
+        s2p_map.insert(*symbol, permutation.to_owned());
     }
 
+    /*
     //[DBG]// print the resulting hashmap "s2p_map"
-    //println!("s2p_map:");
-    //for (symbol, permutation) in s2p_map.iter() {
-    //    println!("{}: {}", symbol, permutation.iter().collect::<String>());
-    //}
+    println!("s2p_map:");
+    for (symbol, permutation) in s2p_map.iter() {
+       println!("{}: {}", symbol, permutation.iter().collect::<String>());
+    }
+    */
 
     //  read "txt2ncrypt"
-    let txt2ncrypt = args.input.map(|s| s.trim_end().to_owned())
-    .unwrap_or_else(|| {
-        println!("please define txt2ncrypt!");
-        print!("txt2ncrypt: ");
-        io::stdout().flush().unwrap();
-        let mut txt2ncrypt = String::new();
-        match io::stdin().read_line(&mut txt2ncrypt) {
-            Ok(_) => txt2ncrypt.trim_end().to_owned(),
-            Err(err) => {
-                eprintln!("Error reading UserInputStringTXT2NCRYPT: {}", err);
-                std::process::exit(1);
+    let txt2ncrypt = args
+        .input
+        .map(|s| s.trim_end().to_owned())
+        .unwrap_or_else(|| {
+            println!("please define txt2ncrypt!");
+            print!("txt2ncrypt: ");
+            io::stdout().flush().unwrap();
+            let mut txt2ncrypt = String::new();
+            match io::stdin().read_line(&mut txt2ncrypt) {
+                Ok(_) => txt2ncrypt.trim_end().to_owned(),
+                Err(err) => {
+                    eprintln!("Error reading UserInputStringTXT2NCRYPT: {}", err);
+                    std::process::exit(1);
+                }
             }
-        }
-    });
-    
+        });
 
     //  encrypt "txt2ncrypt" using "s2p_map"
-    let txt2dcrypt = txt2ncrypt.chars()
-    .map(|c| {
-        s2p_map.get(&c).unwrap().iter().collect::<String>()
-    })
-    .collect::<String>();
+    let txt2dcrypt = txt2ncrypt
+        .chars()
+        .map(|c| s2p_map.get(&c).unwrap().iter().collect::<String>())
+        .collect::<String>();
 
     //  print "txt2dcrypt"
     println!("txt2dcrypt: {}", txt2dcrypt);
 
-    //  call 'img_generator' with 'txt2dcrypt' to create icon image 'img2dcrypt' and save it 
-    img_generator(&txt2dcrypt, &s2p_map);
+    //  call 'img_generator' with 'txt2dcrypt' to create icon image 'img2dcrypt' and save it
+    img_generator(&txt2dcrypt);
 
-Ok(())
+    Ok(())
 }
 
 //Z85
